efe73b3d2d4679e6fb3059c9bb41e2572f8b52c0
evilqueen599 <triciaalhammadi@gmail.com>
2022-06-05 16:37:17 -0400

    update

--- app/src/main/java/com/example/c196aloufi/TermList.java
+++ app/src/main/java/com/example/c196aloufi/TermList.java
@@ -1,6 +1,12 @@
 package com.example.c196aloufi;
 
 import android.app.Activity;
+import android.os.Bundle;
+
 
 public class TermList extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
 }
