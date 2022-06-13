1771c7e6b97cea0eb5b85988eda4cced48a9dd4d
evilqueen599 <triciaalhammadi@gmail.com>
2022-06-13 17:48:50 -0400

    update

---
+++ app/src/main/java/com/example/c196aloufi/UserInterface/AddTerm.java
@@ -0,0 +1,15 @@
+package com.example.c196aloufi.UserInterface;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.c196aloufi.R;
+
+public class AddTerm extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_term);
+    }
+}
--- app/src/main/java/com/example/c196aloufi/UserInterface/DetailedTerm.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/DetailedTerm.java
@@ -1,6 +1,8 @@
 package com.example.c196aloufi.UserInterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class DetailedTerm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_term_list);
+    }
+
+    public void onClickAddTerm(View view) {
+        Intent intent = new Intent(DetailedTerm.this, AddTerm.class);
+        startActivity(intent);
     }
 }
