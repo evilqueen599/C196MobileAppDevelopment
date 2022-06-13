e155277e6a2c39584b80b44f6c1a087396391598
evilqueen599 <triciaalhammadi@gmail.com>
2022-06-12 23:03:02 -0400

    update

--- app/src/main/java/com/example/c196aloufi/UserInterface/TermList.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/TermList.java
@@ -2,13 +2,31 @@ package com.example.c196aloufi.UserInterface;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.c196aloufi.R;
 
 
 public class TermList extends Activity {
+    private TextView termListTxt;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+    }
+    public void onClickTerms(View view) {
+        TextView myTextView = (TextView) findViewById(R.id.termListTxt);
+        myTextView.setText("Term List");
+    }
+
+    public void onClickCourses(View view) {
+        TextView myTextView = (TextView) findViewById(R.id.termListTxt);
+        myTextView.setText("Course List");
+    }
+
+    public void onClickAssess(View view) {
+        TextView myTextView = (TextView) findViewById(R.id.termListTxt);
+        myTextView.setText("Assessment List");
     }
 }
