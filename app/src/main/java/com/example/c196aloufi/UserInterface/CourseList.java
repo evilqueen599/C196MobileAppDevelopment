389e47dfabb7dd88e4b583e0cdacb35f37027978
evilqueen599 <triciaalhammadi@gmail.com>
2022-06-26 17:36:25 -0400

    update

--- app/src/main/java/com/example/c196aloufi/UserInterface/CourseList.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/CourseList.java
@@ -3,6 +3,7 @@ package com.example.c196aloufi.UserInterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.c196aloufi.R;
 
@@ -11,5 +12,8 @@ public class CourseList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list);
+    }
+
+    public void addACourseBtn(View view) {
     }
 }
