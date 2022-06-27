f723434032aa7983d371a509f2c7945e090cb4a0
evilqueen599 <triciaalhammadi@gmail.com>
2022-06-26 23:54:10 -0400

    update

--- app/src/main/java/com/example/c196aloufi/UserInterface/CourseList.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/CourseList.java
@@ -2,12 +2,14 @@ package com.example.c196aloufi.UserInterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.c196aloufi.R;
 
-public class CourseList extends AppCompatActivity {
+public class CourseList extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,12 @@ public class CourseList extends AppCompatActivity {
     }
 
     public void addACourseBtn(View view) {
+        Intent intent = new Intent(CourseList.this, AddCourse.class);
+        startActivity(intent);
+    }
+
+    public void addCourseBackButton(View view) {
+        Intent intent = new Intent (CourseList.this, TermList.class);
+        startActivity(intent);
     }
 }
