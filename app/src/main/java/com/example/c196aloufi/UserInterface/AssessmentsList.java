8efad454fabe91e80190d5a4b39cb11df12ab54b
evilqueen599 <triciaalhammadi@gmail.com>
2022-07-01 21:04:53 -0400

    update

--- app/src/main/java/com/example/c196aloufi/UserInterface/AssessmentsList.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/AssessmentsList.java
@@ -1,11 +1,19 @@
 package com.example.c196aloufi.UserInterface;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.c196aloufi.R;
+
 public class AssessmentsList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessments_list);
+    }
     public void addAssessBackButton(View view) {
         Intent intent = new Intent(AssessmentsList.this, DetailedTerm.class);
         startActivity(intent);
