1fe69cfe6a7c2b1ffefb7727759606db9470b5da
evilqueen599 <triciaalhammadi@gmail.com>
2022-10-02 01:38:55 -0400

    update

--- app/src/main/java/com/example/c196aloufi/UserInterface/AssessmentsList.java
+++ app/src/main/java/com/example/c196aloufi/UserInterface/AssessmentsList.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c196aloufi.Adapters.AssessmentAdapter;
+import com.example.c196aloufi.Adapters.TermAdapter;
+import com.example.c196aloufi.Database.AppRepo;
+import com.example.c196aloufi.Model.Assessments;
+import com.example.c196aloufi.Model.Terms;
 import com.example.c196aloufi.R;
+
+import java.util.List;
 
 public class AssessmentsList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessments_list);
-    }
 
-    public void addAssessBackButton(View view) {
-        Intent intent = new Intent(AssessmentsList.this, mainScreen.class);
-        startActivity(intent);
+        RecyclerView recyclerView = findViewById(R.id.assessmentsRecyclerView);
+        AppRepo appRepo = new AppRepo(getApplication());
+        List<Assessments> assessments = appRepo.getAllAssessments();
+        final AssessmentAdapter assessmentAdapter = new AssessmentAdapter(this);
+        recyclerView.setAdapter(assessmentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        assessmentAdapter.setAssessments(assessments);
     }
 
     public void addAssessBtn(View view) {
